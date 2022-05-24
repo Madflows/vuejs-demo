a69899af27555cb41c299fd83d29bcc29deda688
Folarin Lawal <lawalfolarin6@gmail.com>
2022-05-24 15:12:03 +0000

    initialized project

---
+++ app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    // The Data Function
+    data() {
+        return {
+            title: 'The Final Empire',
+            author: 'Robert Weird'
+        }
+    }
+})
+
+app.mount('#app')
